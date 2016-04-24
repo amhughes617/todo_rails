1372bd2bbd986a983f59f904277cbd53a6503cf4
amhughes617 <amhughes@g.cofc.edu>
2016-04-24 14:12:34 -0400

    added a config line for rspec to make 2.99 expect work

---
+++ spec/features/todo_lists/create_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe "Creating todo lists" do
+  it "redirects to the todo list index page on success" do
+    visit "/todo_lists"
+    click_link "New Todo list"
+    expect(page).to have_content("New Todo List")
+
+    fill_in "Title", with: "My todos"
+    fill_in "Description", with: "This is what I'm doing today"
+    click_button "Create Todo list"
+    expect(page).to have_content("My todos")
+  end
+end
